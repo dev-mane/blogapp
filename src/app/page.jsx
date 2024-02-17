36c34eb7b0976e7516737a3dc3c834e88bae74e5
dev-mane <dmhlabane2@gmail.com>
2024-02-17 17:22:55 +0000

    edited files and folders in app and components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,22 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/Menu/Menu";
 
-export default function Home() {
+
+export default function Home({ searchParams }) {
+  const page = parseInt(searchParams.page) || 1
+
   return (
-    <div>
-      <Link href="/">Hello</Link>
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList page={page}/>
+        <Menu />
+      </div>      
     </div>
   );
 }
